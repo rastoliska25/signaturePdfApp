801ff9b722fa6cfc7ed423dec4673dc8cdd283e4
rastoliska25 <rasto.liska25@gmail.com>
2022-09-14 16:19:23 +0200

    removing unecessary classes

--- src/main/java/signature/controller/TestController.java
+++ src/main/java/signature/controller/TestController.java
@@ -1,20 +1,15 @@
 package signature.controller;
 
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import signature.Logging;
-import signature.model.FileDownloadUtil;
 import signature.model.FileEdit;
 import signature.model.FileUploadResponse;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -54,15 +49,6 @@ public class TestController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-
-    @GetMapping("/test2")
-    public ResponseEntity<String> uploadFiles() throws IOException {
-
-        //FileEdit.editFile2();
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
     @GetMapping("/downloadFile")
     public ResponseEntity<String> uploadFiles2() throws IOException {
 
@@ -88,30 +74,6 @@ public class TestController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @GetMapping("/downloadFile/{fileCode}")
-    public ResponseEntity<?> downloadFile(@PathVariable("fileCode") String fileCode) {
-        FileDownloadUtil downloadUtil = new FileDownloadUtil();
-
-        Resource resource = null;
-        try {
-            resource = downloadUtil.getFileAsResource(fileCode);
-        } catch (IOException e) {
-            return ResponseEntity.internalServerError().build();
-        }
-
-        if (resource == null) {
-            return new ResponseEntity<>("File not found", HttpStatus.NOT_FOUND);
-        }
-
-        String contentType = "application/octet-stream";
-        String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
-                .body(resource);
-    }
-
     @PostMapping("/receiveImage")
     public ResponseEntity<FileUploadResponse> uploadImages(@RequestParam("file") MultipartFile multipartFile) throws IOException {
 
@@ -127,7 +89,6 @@ public class TestController {
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
-
 
 
 }
